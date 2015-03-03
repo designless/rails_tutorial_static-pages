70d325c24c28a6f92b60ef0ab0ee7859a5f4050a
Naoki Kanazawa <info@designless.net>
2015-03-03 14:08:31 +0900

    Add gravatar and Fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resource :users
+  resources :users
 
   root 'static_pages#home'
-  # match '/', to: 'static_pages#home', via: 'get'
-
   match '/signup',  to: 'users#new', via: 'get'
   match '/help', to:'static_pages#help', via:'get'
   match '/about', to:'static_pages#about', via:'get'
